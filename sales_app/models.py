030c8927dbd7f9cd11e10d79a0c7d4b0e8c67c35
Adem <ado.designs@outlook.de>
2025-10-21 07:00:21 +0200

    feat: Models and Database

--- sales_app/models.py
+++ sales_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    newsletter = models.BooleanField(default=True)
+    email = models.EmailField(default="", blank=True)
+    account = models.FloatField(blank=True, null=True)
