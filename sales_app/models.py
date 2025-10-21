8a1b618548a5487a5fcbb659b839073fce758274
Adem <ado.designs@outlook.de>
2025-10-21 15:20:30 +0200

    feat: relationships

--- sales_app/models.py
+++ sales_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
@@ -9,3 +7,38 @@ class Customer(models.Model):
     newsletter = models.BooleanField(default=True)
     email = models.EmailField(default="", blank=True)
     account = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
+
+
+class Bill(models.Model):
+    total_amount = models.FloatField()
+    is_paid = models.BooleanField(default=False)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='Producttype')
+    bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
+
+
+class Producttype(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    type_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.type_name}"
+
+    def save(self, *args, **kwargs):
+        print("saved")
+        super().save(*args, **kwargs)
