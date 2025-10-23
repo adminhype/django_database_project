e4d63eee5816d427748b58651daf7e9a62964dc0
Adem <ado.designs@outlook.de>
2025-10-23 16:47:44 +0200

    feat: adminpanel register admin and expand Customer

--- sales_app/admin.py
+++ sales_app/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Customer, Product, Bill, Order, Producttype
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_filter = ["first_name", "last_name", "newsletter"]
+    # list_display = ["first_name", "last_name",
+    # "newsletter", "email", "account"]
+
+    # readonly_fields = ["account"]
+
+    prepopulated_fields = {"slug": ["first_name", "last_name"]}
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "first_name",
+                    "last_name",
+                    "account"
+                ]
+            },
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": [
+                    "newsletter", "slug"
+                ],
+            },
+        ),
+    ]
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product)
+admin.site.register(Bill)
+admin.site.register(Order)
+admin.site.register(Producttype)
